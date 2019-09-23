66ba173775e06cac3075cd2e6575865ac1af2ffd
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 15:14:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(instructors, steven)
   no_offense_steven=instructors.delete("Steven")
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, robocop)
   deleted_robot=famous_robots.delete_at(2)
 end
