43f46499527087f71bc96c3599c3f97b10f9e0e8
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 15:11:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,7 +38,7 @@ def using_flatten(instruments)
   flat_array = instruments.flatten
 end
 
-def using_delete(instructors, "Steven")
+def using_delete(instructors, )
   no_offense_steven=instructors.delete
 end
 
