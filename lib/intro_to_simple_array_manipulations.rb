65448b4b9593aa78db82095cd90be003c71221ca
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 14:43:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,7 @@ def using_concat(my_favorite_things)
 end
 
 def using_insert(list_of_programming_languages, "Python")
-  list_of_programming_languages.insert("Python")
+  new_array = list_of_programming_languages.insert("Python")
 end
 
 def using_uniq(haircuts)
