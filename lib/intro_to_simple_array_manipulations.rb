4be378dfd192906bad30a739bfde8ec2e57386e9
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 14:06:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,5 +15,6 @@ end
 
 def pop_with_args(dog_breeds)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  small_dogs = dog_breeds.pop
+  shiba_inu = dog_breeds.pop
+  
 end
