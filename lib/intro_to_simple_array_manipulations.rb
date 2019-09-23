270b159fd2b0babc9973d65a7337c3ee53230aa0
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 15:12:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,10 +38,10 @@ def using_flatten(instruments)
   flat_array = instruments.flatten
 end
 
-def using_delete(instructors, steven )
+def using_delete(instructors, steven)
   no_offense_steven=instructors.delete
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, 2)
   deleted_robot=famous_robots.delete_at(2)
 end
