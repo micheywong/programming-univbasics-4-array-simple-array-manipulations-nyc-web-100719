faa2c4f043d64d059c6700e268b876b9236148b0
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 15:01:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ def shift_with_args(ice_cream_brands)
   brands_removed = ice_cream_brands.shift(2)
 end
 
-def using_concat(my_favorite_things, all_my_favs)
+def using_concat(my_favorite_things, more_favs)
   all_my_favs = my_favorite_things + more_favs
 end
 
