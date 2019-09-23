805e45385ffca6f1612979349905ebf8d3982dc0
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 15:00:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,7 +23,7 @@ def shift_with_args(ice_cream_brands)
 end
 
 def using_concat(my_favorite_things, all_my_favs)
-  all_my_favs = my_favorite_things.concat
+  all_my_favs = my_favorite_things + more_favs
 end
 
 def using_insert(list_of_programming_languages, another_language)
