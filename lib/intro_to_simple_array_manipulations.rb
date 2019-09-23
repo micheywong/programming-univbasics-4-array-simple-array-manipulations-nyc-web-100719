62a9176028670ca039014b584f89c92fc027f26d
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 14:13:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,4 +20,6 @@ def pop_with_args(dog_breeds)
   
 end
 
-def using_shift
+def using_shift(my_favorite_cities)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  
