6284c26c6a5eb2acb3e4ea4dd43c1ba0a9505bf8
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 14:57:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,9 +11,7 @@ def using_pop(continents)
 end
 
 def pop_with_args(dog_breeds)
-  
   small_dogs = dog_breeds.pop(2)
-
 end
 
 def using_shift(my_favorite_cities)
@@ -21,7 +19,7 @@ def using_shift(my_favorite_cities)
 end
 
 def shift_with_args(ice_cream_brands)
-  brands_removed = ice_cream_brands.shift
+  brands_removed = ice_cream_brands.shift(2)
 end
 
 def using_concat(my_favorite_things)
