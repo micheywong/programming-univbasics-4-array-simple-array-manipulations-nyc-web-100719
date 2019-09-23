4c55488fdec9bac1c98fa0b515141dffd39153a5
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 14:32:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,27 +1,22 @@
 def using_push(colors_in_the_rainbow, string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   colors_in_the_rainbow.push("violet")
 end
 
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.unshift("Staten Island")
 end
 
 def using_pop(continents)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   continents.pop
 end
 
 def pop_with_args(dog_breeds)
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
   shiba_inu = dog_breeds.pop
   chihuahua = dog_breeds.pop
   
 end
 
 def using_shift(my_favorite_cities)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   im_so_over_this_city = my_favorite_cities.shift
 end
 
